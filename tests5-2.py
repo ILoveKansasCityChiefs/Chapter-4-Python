3d6ff6651165fa98c9db25a8d7ef4d1513329853
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-08 10:51:26 -0400

    Add tests for string and numerical comparisons

---
+++ tests5-2.py
@@ -0,0 +1,20 @@
+car = 'Lambo'
+# test for equality using strings
+print(car == 'audi')
+# test for inequality using strings
+print(car != 'audi')
+# numerical test involving equality
+print(5 == 5)
+# Numerical test involving inequality
+print(45 != 5 ** 5)
+# NUmerical test involving symbols 1
+print(65 >= 100)
+# Numerical test involving symbols 2 
+print(100 >= 75000)
+
+subjects = ['Math', 'ELA', 'Science', 'Social Studies', 'AI', 'PE']
+'Math' in subjects
+print("True")
+
+'ELA' in subjects
+print("False")
