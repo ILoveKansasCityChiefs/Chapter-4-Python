2b7ed0e5b2e65cad04a83535f4b457aae4abb18b
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-08 11:30:24 -0400

    Add alien color point system

---
+++ aliencolors5-3,4,5.py
@@ -0,0 +1,7 @@
+alien_color = 'red'
+if alien_color == 'green':
+    print("You have just earned 5 points!")
+elif alien_color == 'yellow':
+    print("ou have just earned 10 points!")
+else:
+    print("You have just earned 15 points!")
