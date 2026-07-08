1002411deef707d98a6d3d036d7e57db6c0892f9
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-08 12:04:46 -0400

    Add greeting logic for usernames in usernames5-8.py

---
+++ usernames5-8.py
@@ -0,0 +1,7 @@
+usernames = ["Jaden", "Admin", "Jake", "Josiah", "Jacuzzi"]
+for username in usernames:
+     if username == "Admin":
+        print("Greetings Admin! Would you like to see reports?")
+     else:
+        
+        print(f"Hello {username}! Welcome Back!")
