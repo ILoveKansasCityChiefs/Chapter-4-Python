94225bfc770aef4df1cdf9011676dbeabe00515d
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-08 11:40:35 -0400

    Add favorite fruits check and print messages

---
+++ favoritefruit5-7.py
@@ -0,0 +1,7 @@
+favorite_fruits = ["Watermelon", "Mango", "Grapes"]
+if 'Watermelon' in favorite_fruits:
+    print("Well Well Well")
+if 'Mango' in favorite_fruits:
+    print("Mango Bango Lover")
+if 'Grapes' in favorite_fruits:
+    print("Grape Lover? Ew worst flavor OAT")
