c1254eea8cceb9f5690af05594e293464fa4fb5c
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-08 10:38:16 -0400

    Add QB prediction logic for Lamar Jackson

---
+++ Quarterbacks5-1.py
@@ -0,0 +1,9 @@
+nfl_QB = 'Lamar Jackson'
+print("Is your QB Lamar Jackson? I predict true")
+print(nfl_QB == 'Lamar Jackson')
+
+print("\nIs your QB Patrick Mahomes? I predict false")
+print(nfl_QB == 'Patrick Mahomes')
+
+print("\nIs your QB Brock Purdy? I predict false")
+print(nfl_QB == 'Brock Purdy')
